eb92fec41831a7a72ed874efaa1879486e3defcd
Vasiliy R <ya-kubanec@yandex.ru>
2024-07-28 16:18:08 +0300

    Update migrations (fix "s" bug)

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2024_07_28_093827) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
